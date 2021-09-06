2e487bab7e1c47b4619ea1d2c83fe6cc35af8357
eisbilen <erdemisbilen@gmail>
2021-09-06 23:22:37 +0100

    commit on main

--- sent_to_sql.py
+++ sent_to_sql.py
@@ -3,9 +3,13 @@ import json
 import re
 
 import time
+
+import shutil
+import os
+
+
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-
 
 
 def remove_puct(sentence):
@@ -17,11 +21,11 @@ def sentence_length_calc(sentence):
     return sentence_length
 
 def write_to_sql(filename):
-    connection = sqlite3.connect("/data/lingomooAPP.db")
+    connection = sqlite3.connect("/data/database/lingomooAPP.db")
     cursor = connection.cursor()
 
     command1 = """CREATE TABLE IF NOT EXISTS
-    sentences(sentence_id INTEGER PRIMARY KEY, date_scrapped TEXT, article_tag TEXT, article_url TEXT, website TEXT, sentence TEXT, sentence_length INTEGER, difficulty_score FLOAT)"""
+    sentences(sentence_id INTEGER PRIMARY KEY, date_scrapped TEXT, article_tag TEXT, article_url TEXT, article_image_src TEXT, website TEXT, sentence TEXT, sentence_length INTEGER, difficulty_score FLOAT)"""
 
     cursor.execute(command1)
 
@@ -32,7 +36,8 @@ def write_to_sql(filename):
         for l in data:
             date_scrapped = l['datetime']
             article_tag = l['tag']
-            article_url = l['link']
+            article_url = l['article_url']
+            article_image_src = l['article_image_src']
             website = l['website']
 
             for sentence_article in l['article']:
@@ -42,14 +47,18 @@ def write_to_sql(filename):
                 print(sentence)
                 # only adding the sentences with the word count between 7 and 15
                 if sentence_length > 7 and sentence_length < 15:
-                    cursor.execute("INSERT INTO sentences (date_scrapped, article_tag, article_url, website, sentence, sentence_length, difficulty_score) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                    (date_scrapped, article_tag, article_url, website, sentence, sentence_length, difficulty_score))
+                    cursor.execute("INSERT INTO sentences (date_scrapped, article_tag, article_url, article_image_src, website, sentence, sentence_length, difficulty_score) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                    (date_scrapped, article_tag, article_url, article_image_src, website, sentence, sentence_length, difficulty_score))
                     connection.commit()
 
     # deletes the dublicate lines containing the same sentece
     cursor.execute("DELETE FROM sentences WHERE sentence_id NOT IN (SELECT min(sentence_id) FROM sentences GROUP BY sentence)")
     connection.commit() 
     cursor.close()
+
+    shutil.move(filename, "/data/archive/json/" + os.path.basename(filename))
+
+
 
 if __name__ == "__main__":
     patterns = ["*.json"]
@@ -64,7 +73,7 @@ if __name__ == "__main__":
 
     my_event_handler.on_created = on_created
 
-    path = "/data/"
+    path = "/data/today/json/"
     go_recursively = True
     my_observer = Observer()
     my_observer.schedule(my_event_handler, path, recursive=go_recursively)
@@ -76,8 +85,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         my_observer.stop()
         my_observer.join()
-
-
-
-
-
